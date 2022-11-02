3bf5081fb16ecf188229c1bffabb00d8aa45fac1
tommygz8387 <tommy.gz.8387@gmail.com>
2022-11-02 21:54:24 +0700

    hmmm

--- database/factories/AmpuFactory.php
+++ database/factories/AmpuFactory.php
@@ -14,7 +14,7 @@ class AmpuFactory extends Factory
     public function definition()
     {
         return [
-            'guru_id' => $this->faker->numberBetween(1, 10),
+            'guru_id' => $this->faker->numberBetween(1, 50),
             'mapel_id' => $this->faker->numberBetween(1, 15),
             'tingkat_id' => $this->faker->numberBetween(1, 3),
             'created_at' => now(),
